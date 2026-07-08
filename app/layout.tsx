95d02a99b785aa7dbd57155c09796d05e68c5351
impradumanrana <praduman508@gmail.com>
2026-07-09 00:16:35 +0530

    Test runner 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Light Dashboard",
+  title: "YO YO TEST  Dashboard",
   description: "A simple dashboard with dummy business data",
 };
 
